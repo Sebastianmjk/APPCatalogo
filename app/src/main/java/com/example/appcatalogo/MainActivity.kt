7dc4e5195d5286d57e31eb25b3e32a06de654ca1
unknown <usuario1@M2-305-20-11379.utadeo.loc>
2024-04-02 12:56:21 -0500

    Primera actividad

--- app/src/main/java/com/example/appcatalogo/MainActivity.kt
+++ app/src/main/java/com/example/appcatalogo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.appcatalogo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val comienzaButton =  findViewById<Button>(R.id.button);
+        comienzaButton.setOnClickListener{
+            val intent = Intent(this,Main2Activity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
 }
