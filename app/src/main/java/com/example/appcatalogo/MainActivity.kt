b16663980c9810b66879098442b1c78d290842b6
mojicasebastian67@gmail.com <mojicasebastian67@gmail.com>
2024-04-08 14:52:45 -0500

    Fragmento registro

--- app/src/main/java/com/example/appcatalogo/MainActivity.kt
+++ app/src/main/java/com/example/appcatalogo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
