cbb0c8b27ce5905e418c85e62853bd19352afa58
AC-Santiago <acostasantiago311124578@gmail.com>
2024-04-09 12:17:31 -0500

    Se modifico lo .kt del registro

--- app/src/main/java/com/example/appcatalogo/SignUp/SignUp.kt
+++ app/src/main/java/com/example/appcatalogo/SignUp/SignUp.kt
@@ -2,15 +2,12 @@ package com.example.appcatalogo.SignUp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.appcatalogo.R
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-
 
     }
 }
--- app/src/main/java/com/example/appcatalogo/SignUp/registroFirstPage.kt
+++ app/src/main/java/com/example/appcatalogo/SignUp/registroFirstPage.kt
@@ -19,4 +19,5 @@ class registroFirstPage : Fragment() {
     }
 
 
+
 }
