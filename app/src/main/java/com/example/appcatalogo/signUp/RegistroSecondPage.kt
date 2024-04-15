968ae182dfd06190ef844fa35728cea025287170
mojicasebastian67@gmail.com <mojicasebastian67@gmail.com>
2024-04-15 17:15:32 -0500

    Pruebas segunda pagina registro

--- app/src/main/java/com/example/appcatalogo/signUp/RegistroSecondPage.kt
+++ app/src/main/java/com/example/appcatalogo/signUp/RegistroSecondPage.kt
@@ -12,8 +12,7 @@ import com.example.appcatalogo.R
 class RegistroSecondPage : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registro_second_page, container, false)
