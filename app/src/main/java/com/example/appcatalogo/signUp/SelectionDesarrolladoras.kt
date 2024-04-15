027fa2c50995bbcbbf55f85d05610e5b0286e063
mojicasebastian67@gmail.com <mojicasebastian67@gmail.com>
2024-04-15 17:57:40 -0500

    Navegacion incompleta

--- app/src/main/java/com/example/appcatalogo/signUp/SelectionDesarrolladoras.kt
+++ app/src/main/java/com/example/appcatalogo/signUp/SelectionDesarrolladoras.kt
@@ -1,32 +1,29 @@
 package com.example.appcatalogo.signUp
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.appcatalogo.R
 
-class RegistroFourthPage : Fragment() {
-
+class SelectionDesarrolladoras : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_fourth_page, container, false)
+        return inflater.inflate(R.layout.fragment_selection_desarrolladoras, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val buttonNext = view.findViewById<Button>(R.id.buttonNext)
         val buttonBack = view.findViewById<Button>(R.id.buttonBack)
         buttonNext.setOnClickListener {
-            findNavController().navigate(R.id.action_registroFourthPage_to_knowMore)
+            findNavController().navigate(R.id.action_selectionDesarrolladoras_to_selectionDispositivos)
         }
         buttonBack.setOnClickListener {
             findNavController().navigateUp()
--- app/src/main/java/com/example/appcatalogo/signUp/SelectionDispositivos.kt
+++ app/src/main/java/com/example/appcatalogo/signUp/SelectionDispositivos.kt
@@ -1,23 +1,21 @@
 package com.example.appcatalogo.signUp
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.appcatalogo.R
 
-class RegistroFourthPage : Fragment() {
-
+class SelectionDispositivos : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_fourth_page, container, false)
+        return inflater.inflate(R.layout.fragment_selection_dispositivos, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,7 +24,7 @@ class RegistroFourthPage : Fragment() {
         val buttonNext = view.findViewById<Button>(R.id.buttonNext)
         val buttonBack = view.findViewById<Button>(R.id.buttonBack)
         buttonNext.setOnClickListener {
-            findNavController().navigate(R.id.action_registroFourthPage_to_knowMore)
+            findNavController().navigate(R.id.action_selectionDispositivos_to_seleccionCategorias)
         }
         buttonBack.setOnClickListener {
             findNavController().navigateUp()
