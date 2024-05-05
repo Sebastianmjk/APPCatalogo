d5d4ca5f7c42f6e528c283540bbfdcf838bbf943
juliocontreras03 <julioc.contrerasg@utadeo.edu.co>
2024-05-04 22:24:15 -0500

    meti los fragmentos de registro y el fragmente perfil

---
+++ app/src/main/java/com/example/appcatalogo/login/RegistroCorreoContra.kt
@@ -0,0 +1,39 @@
+package com.example.appcatalogo.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.appcatalogo.R
+import com.example.appcatalogo.databinding.FragmentCorreoContraBinding
+import com.example.appcatalogo.databinding.FragmentCorreoVerificacionBinding
+
+
+class RegistroCorreoContra : Fragment() {
+
+    private var _binding: FragmentCorreoContraBinding? = null
+
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding= FragmentCorreoContraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonNext.setOnClickListener {
+
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/appcatalogo/login/RegistroNuevaContra.kt
@@ -0,0 +1,38 @@
+package com.example.appcatalogo.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.appcatalogo.R
+import com.example.appcatalogo.databinding.FragmentCorreoContraBinding
+import com.example.appcatalogo.databinding.FragmentNuevaContraBinding
+
+
+class RegistroNuevaContra : Fragment() {
+
+    private var _binding: FragmentNuevaContraBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val args: RegistroCorreoVerificacionArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding= FragmentNuevaContraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonNext.setOnClickListener {
+
+        }
+    }
+
+}
